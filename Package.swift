b56fb7eacd61df64ec8c43849b64069dacf242cc
Christian Hoock <9932295+wk1@users.noreply.github.com>
2025-02-17 16:45:38 +0100

    Add package initial code

--- Package.swift
+++ Package.swift
@@ -4,18 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "AnimatedGradient",
+    name: "FunkyGradients",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AnimatedGradient",
-            targets: ["AnimatedGradient"]),
+            name: "FunkyGradients",
+            targets: ["FunkyGradients"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AnimatedGradient"),
+            name: "FunkyGradients"),
 
     ]
 )
---
+++ Sources/FunkyGradients/FunkyGradients.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+public struct FunkyGradients: View {
+  @State private var funkyGradient: Bool = false
+  
+  public var body: some View {
+    ZStack {
+      TabView {
+        LinearGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+          .frame(width: 300, height: 200)
+          .mask(
+            VStack {
+              Text("Swift")
+                .font(.system(size: 72, weight: .bold))
+              Text("Funky Gradients")
+                .font(.custom("Snell Roundhand", size: 32))
+            }
+          )
+        
+        LinearGradient(colors: [.blue, .green], startPoint:
+            .topLeading, endPoint: .bottomTrailing)
+        .hueRotation(.degrees(funkyGradient ? 45 : 0))
+        .onAppear {
+          withAnimation(.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
+            funkyGradient.toggle()
+          }
+        }
+        .ignoresSafeArea()
+      }
+      .tabViewStyle(PageTabViewStyle())
+    }
+    .ignoresSafeArea()
+  }
+}
+
+#Preview("Funky Gradients") {
+  FunkyGradients()
+    .previewDisplayName("Funky Gradients")
+    .previewLayout(.sizeThatFits)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(
+      Color(red: 0x22 / 255, green: 0x21 / 255, blue: 0x2c / 255)
+    )
+}
